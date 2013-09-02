833a36580b5eb8bc8d3fcb0e69c01fb6160b809b
Theodore DeRego <tderego94@gmail.com>
2013-09-02 09:40:55 -1000

    Login! Doesn't work yet but it's getting there

--- main.go
+++ main.go
@@ -1,41 +1,133 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/gorilla/sessions"
+	_ "github.com/mattn/go-sqlite3"
+	"html/template"
+	"log"
 	"net/http"
+	"os"
 )
 
-var store = sessions.NewCookieStore([]byte("something-very-secret"))
+type Context struct {
+	db    *sql.DB
+	store *sessions.CookieStore
+}
 
-func root(w http.ResponseWriter, r *http.Request) {
-	session, err := store.Get(r, "session-name")
+func NewContext(db *sql.DB, store *sessions.CookieStore) *Context {
+	return &Context{db, store}
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request, c *Context) {
+	session, err := c.store.Get(r, "session-name")
 	if err != nil {
-		fmt.Fprintf(w, "Sorry, there's some weird mumbo jumbo happening around here...")
-	} else if auth, set := session.Values["auth"]; set {
-		fmt.Fprintf(w, "<h1>Welcome, %v</h1>", auth)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	} else if user, set := session.Values["user"]; set {
+		fmt.Fprintf(w, "<h1>Welcome, %v</h1>", user)
 	} else {
-		fmt.Fprintf(w, "<h1>You're not logged in!</h1>")
+		renderTemplate(w, "login.html")
 	}
 }
 
-func login(w http.ResponseWriter, r *http.Request) {
-	// Get a session. We're ignoring the error resulted from decoding an
-	// existing session: Get() always returns a session, even if empty.
-	session, _ := store.Get(r, "session-name")
-	// Set some session values.
-	session.Values["auth"] = 12345
-	// Save it.
-	session.Save(r, w)
+func registerHandler(w http.ResponseWriter, r *http.Request, c *Context) {
+	user := r.FormValue("user")
+	pass := r.FormValue("pass")
+
+	register(w, r, c, user, pass)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request, c *Context) {
+	user := r.FormValue("user")
+	pass := r.FormValue("pass")
+
+	if login(w, r, c, user, pass) {
+		fmt.Fprintf(w, "Successfully logged in!")
+	} else {
+		fmt.Fprintf(w, "Login failed.")
+	}
+}
+
+func makeHandler(c *Context, fn func(http.ResponseWriter, *http.Request, *Context)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fn(w, r, c)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", root)
-	http.HandleFunc("/login", login)
+	os.Remove("./foo.db")
+
+	db, err := sql.Open("sqlite3", "./foo.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	c := NewContext(db, sessions.NewCookieStore([]byte("something-very-secret")))
+
+	_, err = c.db.Query("create table users(username text primary key, password text)")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	http.HandleFunc("/", makeHandler(c, rootHandler))
+	http.HandleFunc("/register", makeHandler(c, registerHandler))
+	http.HandleFunc("/login", makeHandler(c, loginHandler))
 
 	fmt.Println("Serving webserver...")
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Printf(err.Error())
 	}
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	t, err := template.ParseFiles(tmpl)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func register(w http.ResponseWriter, r *http.Request, c *Context, user, pass string) bool {
+	_, err := c.db.Exec("insert into users values(?, ?)", user, pass)
+	if err != nil {
+		log.Fatal(err)
+		return false
+	}
+
+	return true
+}
+
+func login(w http.ResponseWriter, r *http.Request, c *Context, user, pass string) bool {
+	rows, err := c.db.Query("select * from users where username=?", user)
+	defer rows.Close()
+	if err != nil {
+		log.Fatal(err)
+		return false
+	}
+	for rows.Next() {
+		var realUser, realPass string
+		rows.Scan(&realUser, &realPass)
+
+		if realUser == user && realPass == pass {
+			// Get a session. We're ignoring the error resulted from decoding an
+			// existing session: Get() always returns a session, even if empty.
+			session, _ := c.store.Get(r, "session-name")
+			// Set some session values.
+			session.Values["user"] = user
+			// Save it.
+			session.Save(r, w)
+
+			return true
+		}
+	}
+
+	return false
 }
