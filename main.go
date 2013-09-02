b6f3b6a761b54e6952d84393870da2e04c6a7c19
Theodore DeRego <tderego94@gmail.com>
2013-09-01 14:27:08 -1000

    Sessions!!!
    This is all so new to me...

--- main.go
+++ main.go
@@ -9,24 +9,33 @@ import (
 var store = sessions.NewCookieStore([]byte("something-very-secret"))
 
 func root(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello, world!</h1>")
+	session, err := store.Get(r, "session-name")
+	if err != nil {
+		fmt.Fprintf(w, "Sorry, there's some weird mumbo jumbo happening around here...")
+	} else if auth, set := session.Values["auth"]; set {
+		fmt.Fprintf(w, "<h1>Welcome, %v</h1>", auth)
+	} else {
+		fmt.Fprintf(w, "<h1>You're not logged in!</h1>")
+	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func login(w http.ResponseWriter, r *http.Request) {
 	// Get a session. We're ignoring the error resulted from decoding an
 	// existing session: Get() always returns a session, even if empty.
 	session, _ := store.Get(r, "session-name")
 	// Set some session values.
-	session.Values["foo"] = "bar"
-	session.Values[42] = 43
+	session.Values["auth"] = 12345
 	// Save it.
 	session.Save(r, w)
 }
 
 func main() {
 	http.HandleFunc("/", root)
-	http.HandleFunc("/login", handler)
+	http.HandleFunc("/login", login)
 
 	fmt.Println("Serving webserver...")
-	http.ListenAndServe("", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
 }
